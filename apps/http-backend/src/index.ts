3d4e37dee04c8294722caa622900bb12c03ccb5b
Mohd-Arzaid <arzaid0101030@gmail.com>
2026-06-13 11:43:55 +0530

    Add JWT authentication to http-backend and ws-backend apps
    - Introduced JWT_SECRET configuration in both backends.
    - Implemented signup and login routes in http-backend with JWT token generation.
    - Added middleware for token verification in http-backend.
    - Updated WebSocket connection handling in ws-backend to validate JWT tokens.
    - Added necessary dependencies for jsonwebtoken and its types.

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,37 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { middleware } from "./middleware";
+import { JWT_SECRET } from "./config";
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+console.log(JWT_SECRET);
+
+app.post("/signup", (req, res) => {
+  // db call
+  res.json({
+    userId: 123,
+  });
+});
+
+app.post("/login", (req, res) => {
+  const userId = 1;
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
+  res.json({
+    token,
+  });
+});
+
+app.post("/create-room", middleware, (req, res) => {
+  res.json({
+    roomId: 123,
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
 });
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,25 @@
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (ws) => {
+wss.on("connection", (ws, req) => {
+  const url = req.url; // ws://localhost:8080?token=123123
+  if (!url) {
+    ws.close();
+    return;
+  }
+
+  const queryParams = new URLSearchParams(url.split("?")[1]); // { token: "123123" }
+  const token = queryParams.get("token") ?? "";
+  const decoded = jwt.verify(token, JWT_SECRET);
+  if (!decoded || !(decoded as JwtPayload).userId) {
+    ws.close();
+    return;
+  }
+
   ws.on("message", () => {
     ws.send("Hello from server");
   });
